7a39e76af1c9d5fb5a562bbb01a47c92130dc255
Rick-Dev-Br <henrique_rickgamer@hotmail.com>
2025-03-16 15:34:04 -0300

    feat testando uma maneira de ambliar a imagem

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+  // Seleciona a imagem pelo ID
+const image = document.getElementById('zoom-1');
+
+  // Adiciona um evento de clique à imagem
+image.addEventListener('click', function() {
+      // Alterna a classe 'zoomed' na imagem
+    this.classList.toggle('zoomed');
+});
+
+  // Seleciona a imagem pelo ID
+const image2 = document.getElementById('zoom-2');
+
+  // Adiciona um evento de clique à imagem
+image2.addEventListener('click', function() {
+      // Alterna a classe 'zoomed' na imagem
+    this.classList.toggle('zoomed');
+});
+
 
